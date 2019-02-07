60c82ffcf6d008213473b9d7d26b5cbbe92d9a75
mitsudome-r <43976834+mitsudome-r@users.noreply.github.com>
2019-02-07 18:02:14 +0900

    output stderr to log file (#3)

---
+++ logger_configs/log_script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd ~/.ros/log
+
+latest=$(basename $(readlink latest))
+
+#get list of directories except latest log directory
+list_dir=$(ls */ -d | grep -v "${latest}\|latest")
+
+#compress directories except latest log
+if [ "${list_dir}" ]; then
+	tar -zcvf log.tar.gz $list_dir
+	rm $list_dir -R
+fi
+
+/usr/sbin/logrotate -f /etc/logrotate.d/roslog_daily
