09aee745611bdf49a187aa00ff8c39d5a36b1fbb
Yuma Nihei <yuma.nihei@tier4.jp>
2019-02-18 15:42:38 +0900

    Fix install-script (#8)

--- logger_configs/install.sh
+++ logger_configs/install.sh
@@ -1,41 +1,33 @@
 #!/bin/sh
 
 #setup logrotate configs
-sed -e "s/username/${SUDO_USER}/g" roslog  > /tmp/roslog
-sed -e "s/username/${SUDO_USER}/g" roslog_directories > /tmp/roslog_directories
-cp /tmp/roslog /etc/logrotate.d/roslog
-cp /tmp/roslog_directories /etc/logrotate.d/roslog_directories
-rm /tmp/roslog /tmp/roslog_directories
+sed -e "s/username/${SUDO_USER}/g" roslog  > /etc/logrotate.d/roslog
+sed -e "s/username/${SUDO_USER}/g" roslog_directories > /etc/logrotate.d/roslog_directories
 
 #set up cron
 crontmpfile="/tmp/crontmp.txt"
-sed_tmp="/tmp/sed_tmp.txt"
 
-crontab -l > ${crontmpfile}
+crontab -u ${SUDO_USER} -l > ${crontmpfile} 2>/dev/null
 
 f=$(cat ${crontmpfile} | grep '/etc/logrotate.d/roslog')
 if [ ${#f} -eq 0 ]; then
 	#set cron if not set yet
 	echo " * * * * * /usr/sbin/logrotate /etc/logrotate.d/roslog" >> ${crontmpfile}
 else
-	#update cron if already set 
+	#update cron if already set
 	f=$(echo "$f" | sed 's/\*/\\*/g')
-	sed -e "s:${f}: * * * * * /usr/sbin/logrotate /etc/logrotate.d/roslog:g" ${crontmpfile} > ${sed_tmp}
-	cp ${sed_tmp} ${crontmpfile}
+	sed -i -e "s:${f}: * * * * * /usr/sbin/logrotate /etc/logrotate.d/roslog:g" ${crontmpfile}
 fi
 
 f=$(cat ${crontmpfile} | grep 'log_script.sh')
 if [ ${#f} -eq 0 ]; then
 	#set cron if not set yet
-	echo " 0 0 * * * sh ~/roslogger/logger_configs/log_script.sh" >> ${crontmpfile}
+	echo " * * * * * sh ~/roslogger/logger_configs/log_script.sh" >> ${crontmpfile}
 else
-	#update cron if already set 
-	f=$(echo "$f" | sed 's/\*/\\*/g')	
-	sed -e "s:${f}: * * * * * sh ~/roslogger/logger_configs/log_script.sh:g" ${crontmpfile}  > ${sed_tmp}
-	cp ${sed_tmp} ${crontmpfile}
+	#update cron if already set
+	f=$(echo "$f" | sed 's/\*/\\*/g')
+	sed -i -e "s:${f}: * * * * * sh ~/roslogger/logger_configs/log_script.sh:g" ${crontmpfile}
 fi
 
 crontab -u ${SUDO_USER} ${crontmpfile} -i
 rm ${crontmpfile}
-rm ${sed_tmp}
-
